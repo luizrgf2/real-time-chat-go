6d1db0c9e926cf38b3ccda2b0d82474e040158d5
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-01-17 18:04:22 -0300

    ✅ TEST: add new test to test if userName is valid

--- backend/internal/app/user/entities/user.entity_test.go
+++ backend/internal/app/user/entities/user.entity_test.go
@@ -8,34 +8,42 @@ import (
 )
 
 func TestCreateValidUser(t *testing.T) {
-	_, err := CreateUser(1, "test@email.com.br", "testePass123")
+	_, err := CreateUser(1, "test@email.com.br", "luizrgfg", "testePass123")
 	assert.Nil(t, err)
 }
 
+func TestCreateUserWithInvalidUsername(t *testing.T) {
+	_, err := CreateUser(1, "test@email.com.br", "l", "testePass123")
+	assert.ErrorIs(t, err, user_errors.ErrShortUserName)
+
+	_, err = CreateWithoutId("test@email.com.br", "l", "testePass123")
+	assert.ErrorIs(t, err, user_errors.ErrShortUserName)
+}
+
 func TestCreateValidUserWithoutId(t *testing.T) {
-	_, err := CreateWithoutId("test@email.com.br", "testePass123")
+	_, err := CreateWithoutId("test@email.com.br", "luizrgfg", "testePass123")
 	assert.Nil(t, err)
 }
 
 func TestCreateUserWithInvalidEmail(t *testing.T) {
-	_, err := CreateUser(1, "invalid", "testPass123")
+	_, err := CreateUser(1, "invalid", "luizrgfg", "testPass123")
 	assert.ErrorIs(t, err, user_errors.ErrInvalidEmail)
 
-	_, err = CreateWithoutId("invalid", "testPass123")
+	_, err = CreateWithoutId("invalid", "luizrgfg", "testPass123")
 	assert.ErrorIs(t, err, user_errors.ErrInvalidEmail)
 }
 
 func TestCreateUserWithEmptyEmail(t *testing.T) {
-	_, err := CreateUser(1, "", "testPass123")
+	_, err := CreateUser(1, "", "luizrgfg", "testPass123")
 	assert.ErrorIs(t, err, user_errors.ErrEmptyEmail)
 
-	_, err = CreateWithoutId("", "testPass123")
+	_, err = CreateWithoutId("", "luizrgfg", "testPass123")
 	assert.ErrorIs(t, err, user_errors.ErrEmptyEmail)
 }
 
 func TestCreateUserWithInvalidPassword(t *testing.T) {
-	_, err := CreateUser(1, "test@email.com.br", "invalid")
+	_, err := CreateUser(1, "test@email.com.br", "luizrgfg", "invalid")
 	assert.ErrorIs(t, err, user_errors.ErrShortPassword)
-	_, err = CreateWithoutId("test@email.com.br", "invalid")
+	_, err = CreateWithoutId("test@email.com.br", "luizrgfg", "invalid")
 	assert.ErrorIs(t, err, user_errors.ErrShortPassword)
 }
