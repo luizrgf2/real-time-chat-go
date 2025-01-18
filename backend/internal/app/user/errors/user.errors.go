d59e662f37cf2acf44da842138feec473d072a7c
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-01-18 14:29:20 -0300

    📦 FEAT: add new errors to use in usecases

--- backend/internal/app/user/errors/user.errors.go
+++ backend/internal/app/user/errors/user.errors.go
@@ -3,11 +3,13 @@ package user_errors
 import "errors"
 
 var (
-	ErrShortUserName    = errors.New("o Nome é muito curto deve ter pelo menos 4 caracteres")
-	ErrInvalidID        = errors.New("o ID é inválido")
-	ErrEmptyEmail       = errors.New("o email não pode ser vazio")
-	ErrInvalidEmail     = errors.New("o email é inválido")
-	ErrShortPassword    = errors.New("a senha deve ter pelo menos 8 caracteres")
-	ErrInvalidCreatedAt = errors.New("a data de criação é inválida")
-	ErrInvalidUpdatedAt = errors.New("a data de atualização é inválida")
+	ErrUserNameAlrearyExists = errors.New("o nome de usuário já existe")
+	ErrEmailAlrearyExists    = errors.New("o email já existe")
+	ErrShortUserName         = errors.New("o Nome é muito curto deve ter pelo menos 4 caracteres")
+	ErrInvalidID             = errors.New("o ID é inválido")
+	ErrEmptyEmail            = errors.New("o email não pode ser vazio")
+	ErrInvalidEmail          = errors.New("o email é inválido")
+	ErrShortPassword         = errors.New("a senha deve ter pelo menos 8 caracteres")
+	ErrInvalidCreatedAt      = errors.New("a data de criação é inválida")
+	ErrInvalidUpdatedAt      = errors.New("a data de atualização é inválida")
 )
